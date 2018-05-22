9ab48384e5d89fc2d898d69d3cb4485a83511919
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-05-21 17:01:45 -0700

    Add styling

--- src/header.js
+++ src/header.js
@@ -1,6 +1,11 @@
 const header = () => {
-  let headerElement = document.createElement("h1")
-  headerElement.textContent = "odin-restaurant"
+  let headerElement = document.createElement("div")
+  headerElement.className = "section"
+
+  let titleElement = document.createElement("h1")  
+  titleElement.textContent = "odin-restaurant"
+  titleElement.className = "title"
+  headerElement.appendChild(titleElement)
   
   return headerElement
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import header from "./header"
 import render from "./render"
+import tabs from "./tabs"
 
-const elements = [header()]
+const tabList = [
+                  {title: "About"}, 
+                  {title: "Menu"},
+                  {title: "Contact"}
+                ]
+
+const elements = [header(), tabs(tabList)]
 
 render(elements)
